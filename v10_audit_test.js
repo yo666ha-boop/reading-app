be82e4845325f0120ac42990ca25121843ab0fad
yo666ha-boop <yo666ha@gmail.com>
2026-08-18 14:57:12 +0900

    Update audit expected NH count after Unit 9-2

--- v10_audit_test.js
+++ v10_audit_test.js
@@ -61,7 +61,7 @@ const errors=[];
 const sun=load(SUN_FILES);
 const nh=load(NH_FILES);
 if(Object.keys(sun).length!==5) fail(`Sunshine count expected 5, got ${Object.keys(sun).length}`,errors);
-if(Object.keys(nh).length!==26) fail(`New Horizon count expected 26, got ${Object.keys(nh).length}`,errors);
+if(Object.keys(nh).length!==27) fail(`New Horizon count expected 27, got ${Object.keys(nh).length}`,errors);
 auditSet('SS',sun,errors);auditSet('NH',nh,errors);
 const all=[...Object.values(sun),...Object.values(nh)];
 const ids=new Map();
