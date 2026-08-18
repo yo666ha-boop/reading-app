1df9e421c2767270b535be1264c4e5fb599fcc9f
yo666ha-boop <yo666ha@gmail.com>
2026-08-18 19:18:45 +0900

    Load interaction metadata chunks without document.write

--- v10_interaction_metadata.js
+++ v10_interaction_metadata.js
@@ -1,6 +1,21 @@
 window.V10_INTERACTION_META=window.V10_INTERACTION_META||{};
-if(typeof document!=='undefined'&&typeof document.write==='function'){
- document.write('<script src="v10_interaction_metadata_initial.js"><\/script>');
- document.write('<script src="v10_interaction_metadata_sun_p1_p3.js"><\/script>');
- document.write('<script>window.V10_INTERACTION_META=Object.assign({},window.V10_INTERACTION_META||{},window.V10_INTERACTION_META_P1P3||{});<\/script>');
-}
+(function(){
+ if(typeof document==='undefined')return;
+ const target=window.V10_INTERACTION_META;
+ const first=document.createElement('script');
+ first.src='v10_interaction_metadata_initial.js';
+ first.onload=()=>{
+  const initial=window.V10_INTERACTION_META||{};
+  window.V10_INTERACTION_META=target;
+  Object.assign(target,initial);
+  const second=document.createElement('script');
+  second.src='v10_interaction_metadata_sun_p1_p3.js';
+  second.onload=()=>{
+   Object.assign(target,window.V10_INTERACTION_META_P1P3||{});
+   window.V10_INTERACTION_META=target;
+   if(typeof window.render==='function')window.render();
+  };
+  document.head.appendChild(second);
+ };
+ document.head.appendChild(first);
+})();
