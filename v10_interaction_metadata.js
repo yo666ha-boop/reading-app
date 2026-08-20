95c4dee835b6c401ff04d5bef39d1cfceb6da3c9
yo666ha-boop <yo666ha@gmail.com>
2026-08-20 12:03:26 +0900

    Load manual vocabulary and slash audit overrides 041-050

--- v10_interaction_metadata.js
+++ v10_interaction_metadata.js
@@ -41,7 +41,8 @@ window.V10_INTERACTION_META=window.V10_INTERACTION_META||{};
   ['v10_vocab_slash_manual_004_010.js',()=>({})],
   ['v10_vocab_slash_manual_011_020.js',()=>({})],
   ['v10_vocab_slash_manual_021_030.js',()=>({})],
-  ['v10_vocab_slash_manual_031_040.js',()=>({})]
+  ['v10_vocab_slash_manual_031_040.js',()=>({})],
+  ['v10_vocab_slash_manual_041_050.js',()=>({})]
  ];
  let i=0;
  const next=()=>{
