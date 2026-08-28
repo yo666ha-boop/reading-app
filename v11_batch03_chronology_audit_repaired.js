a103fe76a7e0417ac6cd6dc14339e455efd3a329
yo666ha-boop <yo666ha@gmail.com>
2026-08-28 19:02:19 +0900

    v11: align Batch03 repaired chronology runner with final loader

--- v11_batch03_chronology_audit_repaired.js
+++ v11_batch03_chronology_audit_repaired.js
@@ -1,7 +1,1 @@
-const fs=require('fs');
-let src=fs.readFileSync('v11_batch03_chronology_audit.js','utf8');
-const needle="'v11_batch03_length_repair.js'])";
-const repl="'v11_batch03_length_repair.js','v11_batch03_chronology_repair.js','v11_batch03_chronology_repair_r2.js'])";
-if(!src.includes(needle))throw new Error('Batch03 chronology loader signature changed');
-src=src.replace(needle,repl);
-eval(src);
+require('./v11_batch03_chronology_audit.js');
