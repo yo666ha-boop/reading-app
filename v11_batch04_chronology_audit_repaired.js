1343250cdf35032736d0498718cc1892b9d140b3
yo666ha-boop <yo666ha@gmail.com>
2026-08-28 19:57:51 +0900

    v11: include Batch04 r2 repair in chronology gate

--- v11_batch04_chronology_audit_repaired.js
+++ v11_batch04_chronology_audit_repaired.js
@@ -1,7 +1,7 @@
 const fs=require('fs');
 let src=fs.readFileSync('v11_batch04_chronology_audit.js','utf8');
 const before="['v11_batch04_passages_draft_g1.js','v11_batch04_passages_draft_g2.js','v11_batch04_passages_draft_g3.js']";
-const after="['v11_batch04_passages_draft_g1.js','v11_batch04_passages_draft_g2.js','v11_batch04_passages_draft_g3.js','v11_batch04_length_repair.js']";
+const after="['v11_batch04_passages_draft_g1.js','v11_batch04_passages_draft_g2.js','v11_batch04_passages_draft_g3.js','v11_batch04_length_repair.js','v11_batch04_length_repair_r2.js']";
 if(!src.includes(before))throw new Error('Batch04 chronology loader signature changed');
-src=src.replace(before,after).replace("detectorVersion:'3.1-v11-b04-initial'","detectorVersion:'3.1-v11-b04-length-repaired'");
+src=src.replace(before,after).replace("detectorVersion:'3.1-v11-b04-initial'","detectorVersion:'3.1-v11-b04-length-r2'");
 eval(src);
