9d5586a390f8511487abd69a78e265e4fbbe079c
yo666ha-boop <yo666ha@gmail.com>
2026-08-29 07:37:45 +0900

    v11: include Batch06 final length repair in chronology gate

--- v11_batch06_chronology_audit.js
+++ v11_batch06_chronology_audit.js
@@ -2,6 +2,6 @@
 const fs=require('fs');
 let src=fs.readFileSync('v11_batch05_chronology_audit.js','utf8');
 src=src.replaceAll('V11_BATCH05','V11_BATCH06').replaceAll('Batch05','Batch06').replaceAll('batch05','batch06');
-src=src.replace("'v11_batch06_passages_draft_g3.js','v11_batch06_grammar_repair.js'","'v11_batch06_passages_draft_g3.js','v11_batch06_draft_repairs.js','v11_batch06_grammar_repair.js','v11_batch06_chronology_repair.js'");
+src=src.replace("'v11_batch06_passages_draft_g3.js','v11_batch06_grammar_repair.js'","'v11_batch06_passages_draft_g3.js','v11_batch06_draft_repairs.js','v11_batch06_grammar_repair.js','v11_batch06_chronology_repair.js','v11_batch06_length_repair.js'");
 src=src.replace('runtime_total=368','runtime_total=418');
 eval(src);
