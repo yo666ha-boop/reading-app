e86ade559674b74dfb2855cadb8d7ec2fda6554f
yo666ha-boop <yo666ha@gmail.com>
2026-08-29 14:41:36 +0900

    v11: rerun Batch09 chronology after final grammar repair

---
+++ v11_batch09_chronology_audit_r3.js
@@ -0,0 +1,5 @@
+'use strict';
+const fs=require('fs');
+let src=fs.readFileSync('v11_batch09_chronology_audit.js','utf8');
+src=src.replace("'v11_batch09_g3_length_repair.js']","'v11_batch09_g3_length_repair.js','v11_batch09_grammar_repair.js','v11_batch09_grammar_repair_r2.js']");
+eval(src);
